6b9693b107e7eef674f0274b1186bcbb00c9b5e8
dispet <liftesk.@gmail.com>
2021-03-28 16:47:13 +0300

    feat: update setting.router

--- src/resources/settings/setting.router.js
+++ src/resources/settings/setting.router.js
@@ -1,17 +1,22 @@
-const { OK } = require('http-status-codes');
-const router = require('express').Router({ mergeParams: true });
+const {OK} = require('http-status-codes');
+const router = require('express').Router({mergeParams: true});
 const settingService = require('./setting.service');
-const { settings } = require('../../utils/validation/schemas');
-const { validator } = require('../../utils/validation/validator');
+const {settings} = require('../../utils/validation/schemas');
+const {validator} = require('../../utils/validation/validator');
 
 router.get('/', async (req, res) => {
-  const setting = await settingService.get(req.userId);
-  res.status(OK).send(setting.toResponse());
+    const setting = await settingService.get(req.userId);
+    res.status(OK).send(setting.toResponse());
+});
+
+router.post('/', validator(settings, 'body'), async (req, res) => {
+    const setting = await settingService.upsert(req.userId, req.body);
+    res.status(OK).send(setting.toResponse());
 });
 
 router.put('/', validator(settings, 'body'), async (req, res) => {
-  const setting = await settingService.upsert(req.userId, req.body);
-  res.status(OK).send(setting.toResponse());
+    const setting = await settingService.upsert(req.userId, req.body);
+    res.status(OK).send(setting.toResponse());
 });
 
 module.exports = router;
